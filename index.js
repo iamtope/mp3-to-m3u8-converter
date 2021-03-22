6a3e23a7547a243e36b38009f980a406c55d2379
iamtope <adamoabasstope@yahoo.com>
2021-03-22 01:37:29 +0100

    chnaged var to let

--- index.js
+++ index.js
@@ -1,72 +1,73 @@
-const express = require('express')
+const express = require('express');
 
-const fs = require('fs')
+const fs = require('fs');
 
-const { exec } = require('child_process')
+const { exec } = require('child_process');
 
-const path = require('path')
+const path = require('path');
 
-const multer = require('multer')
+const multer = require('multer');
 
+const app = express();
 
-const app = express()
+let dir = 'public';
+let subDirectory = 'public/uploads';
 
-var dir = 'public';
-var subDirectory = 'public/uploads'
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir);
 
-if (!fs.existsSync(dir)){
-    fs.mkdirSync(dir);
-
-    fs.mkdirSync(subDirectory)
-
+  fs.mkdirSync(subDirectory);
 }
 
-var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'public/uploads')
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
-    }
-})
+let storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/uploads');
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + '-' + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
 
-var upload = multer({storage:storage})
+let upload = multer({ storage: storage });
 
 app.use(express.json());
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.get('/',(req,res) => {
-    res.sendFile(__dirname +'/home.html')
-})
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/home.html');
+});
 
-app.post('/convert',upload.single('file'),(req,res,next) => {
-    if(req.file){
-        console.log(req.file.path)
+app.post('/convert', upload.single('file'), (req, res, next) => {
+  if (req.file) {
 
-        var output = Date.now() + "index.m3u8"
+    let output = Date.now() + 'index.m3u8';
 
-        exec(`ffmpeg -i ${req.file.path} -profile:v baseline -level 3.0 -s 640x360 -start_number 0 -hls_time 10 -hls_list_size 0 -f hls ${output}`, (error, stdout, stderr) => {
-            if (error) {
-                console.log(`error: ${error.message}`);
-                return;
-            }
-            else{
-            res.download(output,(err) => {
-                if(err) throw err
-                
-                fs.unlinkSync(req.file.path)
-                fs.unlinkSync(output)
+    exec(
+      `ffmpeg -i ${req.file.path} -profile:v baseline -level 3.0 -s 640x360 -start_number 0 -hls_time 10 -hls_list_size 0 -f hls ${output}`,
+      (error, stdout, stderr) => {
+        if (error) {
+          console.log(`error: ${error.message}`);
+          return;
+        } else {
+          res.download(output, (err) => {
+            if (err) throw err;
 
-                next()
+            fs.unlinkSync(req.file.path);
+            fs.unlinkSync(output);
 
-            })
+            next();
+          });
         }
-        })
-    }
-})
+      }
+    );
+  }
+});
 
-app.listen(PORT,() => {
-    console.log(`App is listening on Port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`App is listening on Port ${PORT}`);
+});
